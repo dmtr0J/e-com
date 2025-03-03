4106b3559717d0993ab36772ccb27f8792d4d7f6
dmytro <badichel.dmytro@gmail.com>
2025-03-03 21:36:09 +0200

    feat: added header

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from './layouts/header/header.component';
+import {HeroComponent} from './layouts/hero/hero.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, HeroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'frontend';
+  title = 'InkX';
 }
