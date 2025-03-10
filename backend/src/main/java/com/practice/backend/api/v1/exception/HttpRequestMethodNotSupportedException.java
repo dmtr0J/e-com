226c62b0a3e67cd37c0caa3cbf56d82d744adad3
dmytro <badichel.dmytro@gmail.com>
2025-03-10 12:44:26 +0200

    fix: delete unused file

--- backend/src/main/java/com/practice/backend/api/v1/exception/HttpRequestMethodNotSupportedException.java
+++
@@ -1,8 +0,0 @@
-package com.practice.backend.api.v1.exception;
-
-public class HttpRequestMethodNotSupportedException extends RuntimeException {
-
-    public HttpRequestMethodNotSupportedException(String message) {
-        super(message);
-    }
-}
